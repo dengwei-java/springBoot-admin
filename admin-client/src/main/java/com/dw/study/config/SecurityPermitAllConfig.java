a37d5cce928f136913c17484be922fa9e1ff5535
dengwei2 <2546765805@qq.com>
2021-02-07 10:46:57 +0800

    提交

---
+++ admin-client/src/main/java/com/dw/study/config/SecurityPermitAllConfig.java
@@ -0,0 +1,22 @@
+package com.dw.study.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author
+ * @ClassName SecurityPermitAllConfig
+ * @Description
+ * @Date 2021/2/7 9:19
+ * @Version 1.0
+ */
+@Configuration
+public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+}
